c5f0c9f04530fc45233b8146f299de4352e3a85d
remzi.gunes <remzi.gunes@odeontechnology.com>
2022-10-29 07:32:49 +0300

    home page modified and .env added projec

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../utils";
 
 function Home() {
+  const [fetchedData, updateFetchedData] = useState([]);
+
+  useEffect(()=>{
+    fetchCharactersData()
+  },[])
+  const fetchCharactersData = async () => {
+    const api = `${BASE_URL}/character`;
+    const res = await fetch(api).then((res) => res.json());
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter
+          <div className="col-lg-8 col-12">
+            <div className="row">Card Row</div>
+          </div>
+        </div>
+      </div>
+      paging
+    </div>
+  );
 }
 
-export default Home
+export default Home;
